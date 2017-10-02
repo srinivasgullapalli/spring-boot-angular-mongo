8b453757950d30cd5e1128cbb41fe17a3b83de5d
srinivasgullapalli <srini.gullapalli@gmail.com>
2017-10-02 18:27:26 +0400

    delete

--- spring-boot-angularjs-mongo-crud-sample/src/main/java/com/spring/boot/mong/angular/app/repository/IEmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.boot.mong.angular.app.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.repository.CrudRepository;
-
-import com.spring.boot.mong.angular.app.entity.Employee;
-
-public interface IEmployeeRepository extends MongoRepository<Employee, String> {
-}
